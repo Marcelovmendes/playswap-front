c30437368d40095496405dddd004248164479dc2
MarceloMMendes <marcelo.mendes@maxmilhas.com.br>
2025-12-14 23:54:49 -0300

    feat: add initial setup and dependencies

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    domains: ['i.scdn.co'],
+  },
+}
+
+module.exports = nextConfig
